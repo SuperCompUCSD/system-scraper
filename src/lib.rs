9e257cc1c3a22aec0ba369091d3d069f981b967b
GioByte10 <gvanni.bernal10@gmail.com>
2023-03-06 21:37:15 -0800

    top processes with sysinfo

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use sysinfo::{ComponentExt, System, SystemExt};
+use sysinfo::{ComponentExt, System, SystemExt, PidExt, ProcessExt};
+use std::cmp;
 
 /// Returns the current total CPU usage.
 pub fn cpu_usage() -> f32 {
@@ -21,6 +22,16 @@ pub fn cpu_temperature(sys: &System) -> f32 {
 }
 
 /// Returns the command line of top CPU heavy processes.
-pub fn top_processes() -> Vec<(Vec<String>, f32)> {
-	todo!()
+pub fn top_processes(sys: &System, n: u8) -> Vec<(u32, String, f32)> {
+	let mut processes: Vec<(u32, String, f32)> = Vec::new();
+
+	for (pid, process) in sys.processes().iter() {
+		if process.cpu_usage() > 0.0 {
+			processes.push((pid.as_u32(), process.name().to_string(), process.cpu_usage()));
+		}
+	}
+
+	processes.sort_by(|a, b| b.2.partial_cmp(&a.2).unwrap());
+	processes.truncate(cmp::min(n.into(), processes.len()));
+	processes
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 use sysinfo::{System, SystemExt};
-use system_scraper::{cpu_temperature, cpu_usage, load_average, top_processes};
+use system_scraper::{cpu_temperature, top_processes};
 
 fn main() {
 	let mut sys = System::new_all();
-	sys.refresh_all();
+	let mut processes: Vec<(u32, String, f32)>;
+
 	loop {
+		sys.refresh_all();
+
 		//dbg!(cpu_usage());
 		//dbg!(load_average());
-		//dbg!(top_processes());
 		cpu_temperature(&sys);
-		std::thread::sleep(std::time::Duration::from_millis(500));
+		processes = top_processes(&sys, 5);
+
+		for (pid, name, cpu_usage) in processes {
+			println!("{} {} {}", pid, name, cpu_usage);
+		}
+		println!();
+
+		std::thread::sleep(std::time::Duration::from_millis(1000));
 	}
 }
